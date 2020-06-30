00b72fbe03884796d7c20ea54aa16794f8f16fb8
Christian Sheen <christiansheen@Christians-MacBook-Pro.local>
2020-06-30 01:44:45 -0400

    added a few styles a txt input feild which i handle the onChangeText property as well as the onPress property in the Button component

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [enteredGoal, setEnteredGoal] = useState("");
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+	};
+
+	return (
+		<View style={styles.page}>
+			<View style={styles.goalConatainer}>
+				<TextInput
+					placeholder="Course Goal"
+					style={styles.txtInput}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+				/>
+				<Button title="ADD" onPress={addGoalHandler} />
+			</View>
+			<View>
+				{courseGoals.map((goal) => (
+					<View key={goal} style={styles.listItem}>
+						<Text>{goal}</Text>
+					</View>
+				))}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	page: {
+		padding: 30,
+	},
+	goalConatainer: {
+		marginTop: 30,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	txtInput: {
+		height: 40,
+		width: "80%",
+		borderColor: "black",
+		borderWidth: 1,
+		padding: 10,
+		borderRadius: 5,
+	},
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		backgroundColor: "#ccc",
+		borderColor: "black",
+		borderWidth: 1,
+	},
 });
